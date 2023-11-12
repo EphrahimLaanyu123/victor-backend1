421495b2c119ac12109e5b52f93cd5894eb485e3
user <ephrahim.lenaiayasa@student.moringaschool.com>
2023-11-12 16:06:21 +0300

    Added CORS headers

--- app.py
+++ app.py
@@ -4,19 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource, request
 from models import db, Properties
 
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///victor.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 db.init_app(app)
 
 migrate = Migrate(app, db)
 api = Api(app)
 
-
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
 
 class PropertiesList(Resource):
     def get(self):
@@ -39,7 +41,6 @@ class PropertiesList(Resource):
 
         # Return the details of the newly added property
         return new_property.to_dict(), 201  # 201 status code indicates successful creation
-
 
 api.add_resource(PropertiesList, '/properties')
 
